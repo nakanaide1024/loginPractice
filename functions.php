04636ab488474f815305323cb045eb4f8ef7ebe3
nakanaide1024 <71699010+nakanaide1024@users.noreply.github.com>
2021-05-24 18:58:12 +0900

    XSS、エスケープ対策メソッド作成

--- functions.php
+++ functions.php
@@ -1,1 +1,11 @@
-<?php?>
+<?php
+
+/**
+ * XSS対策:エスケープ処理
+ * @param string $str 対象の文字列
+ * @return string 処理された文字列 
+ */
+function h($str){
+  return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
+}
+?>

9b418a1ade607d564d83f621c286cb1d4b012830
nakanaide1024 <71699010+nakanaide1024@users.noreply.github.com>
2021-05-24 19:02:40 +0900

    csrf対策のメソッド作成

--- functions.php
+++ functions.php
@@ -8,4 +8,22 @@
 function h($str){
   return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
 }
+
+/**
+ * CSRF対策
+ * @param void
+ * @return string $csrf_token
+ */
+
+ function setToken(){
+   //トークンを生成
+   //トークンからそのトークンを送信
+   //送信後の画面でそのトークンを照会
+   //トークンを削除
+   session_start();
+   $csrf_token = bin2hex(random_bytes(32));
+   $_SESSION['csrf_token'] = $csrf_token;
+
+   return $csrf_token;
+ }
 ?>
